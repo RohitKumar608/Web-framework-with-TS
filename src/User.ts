466419906a8978c24db9c74299003523c4ce48c2
rohitkumarm <rohitkumarrk13568@gmail.com>
2021-04-04 15:36:13 +0530

    code refactor

--- src/User.ts
+++ src/User.ts
@@ -11,16 +11,4 @@ interface UserProps {
 export class User {
   public events: Eventing = new Eventing()
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl)
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName]
-  }
-
-  set(update: UserProps): void {
-    this.data = {
-      ...this.data,
-      ...update,
-    }
-  }
 }
---
+++ src/models/Attribute.ts
@@ -0,0 +1,14 @@
+export class Attributes<T> {
+  constructor(private data: T) {}
+
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key]
+  }
+
+  set(update: T): void {
+    this.data = {
+      ...this.data,
+      ...update,
+    }
+  }
+}
