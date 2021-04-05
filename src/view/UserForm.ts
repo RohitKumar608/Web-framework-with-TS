022a197a403c0338a69c3fa13bc3ee52ef0655e0
rohitkumarm <rohitkumarrk13568@gmail.com>
2021-04-05 22:46:00 +0530

    added event listener

--- src/view/UserForm.ts
+++ src/view/UserForm.ts
@@ -1,18 +1,40 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:.set-agr': this.handleSetAge,
+      'click:.set-name': this.handleSetName,
+    }
+  }
+
+  handleSetAge(): void {}
+  handleSetName(): void {}
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap()
+    for (const key in eventsMap) {
+      const [event, className] = key.split(':')
+      fragment.querySelectorAll(className).forEach((element) => {
+        element.addEventListener(event, eventsMap[key])
+      })
+    }
+  }
+
   template(): string {
     return `
       <div>
         <h1> User Form </h1>
         <input />
+        <button>Click Here</button>
       </div>
     `
   }
 
   render(): void {
-    const template = document.createElement('template')
-    template.innerHTML = this.template()
-    this.parent.append(template.content)
+    const templateElement = document.createElement('template')
+    templateElement.innerHTML = this.template()
+    this.bindEvents(templateElement.content)
+    this.parent.append(templateElement.content)
   }
 }
